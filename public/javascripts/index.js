a3eef8489c8bc73ad026e1888ef4859d79ef974e
shuaxin <986384495@qq.com>
2021-08-07 10:53:03 +0800

    feat:前端展示&&单独下载完成

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,69 @@
-const Dom = {
-    file: document.querySelector("#file"),
-    dowBtn: document.querySelector("#dowBtn"),
+// 工具库
+const util = {
+    /**
+     *
+     * @param {string} api 接口
+     * @param {Object} opt 请求配置
+     * @returns {Promise}
+     */
+    req(api, opt) {
+        return fetch(api, {
+            ...opt,
+        })
+            .then((res) => {
+                return res.json();
+            })
+            .catch((err) => {
+                return err;
+            });
+    },
+    async dolFile(item) {
+        let data = await fetch(item.url).then((res) => {
+            // 直接返回一个blob即可
+            return res.blob();
+        });
+        var a = document.createElement("a");
+        //文件的名称为时间戳加文件名后缀
+        a.download = item.object;
+        // 创建一个blob链接 理解成类似base64那样的
+        a.href = URL.createObjectURL(data);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    },
 };
-Dom.file.addEventListener("onload", () => {
-        console.log(oFREvent.target.result);
-});
+
+const app = {
+    setup(props) {
+        const state = Vue.reactive({
+            file: null,
+            data: [],
+        });
+        const saveFile = (e) => {
+            state.file = e.target.files;
+        };
+        const send = async () => {
+            if (!state.file || state.file.length === 0) {
+                alert("请选择文件");
+                return;
+            }
+            let formData = new FormData();
+            formData.append("file", state.file[0]);
+            const result = await util.req("http://localhost:3000/sendFile", {
+                method: "POST",
+                body: formData,
+            });
+            state.data = result.data;
+        };
+        const dow = (item) => {
+            util.dolFile(item);
+        };
+        return {
+            send,
+            saveFile,
+            dow,
+            state,
+        };
+    },
+};
+Vue.createApp(app).mount("#app");
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const multiparty = require('multiparty')
-const csvtojson = require('csvtojson');
-const http =require('http')
-const ctj = csvtojson()
-const axios = require('axios')
+const multiparty = require("multiparty");
+const csvtojson = require("csvtojson");
+const http = require("http");
+const axios = require("axios");
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
+router.get("/", function (req, res, next) {
+    res.render("index", {
+        title: "Express",
+    });
 });
 
-router.post('/sendFile', async (req, res, next) => {
-  try {
-    console.log(req.files)
-    let data = await ctj.fromFile(req.files[0].path)
-    res.send({
-      data,
-      total: data.length
-    })
-  } catch (error) {
-    res.send({
-      message:'错误'
-    })
-  }
-  // res.send('上传成功')
-})
+const format = async (data) => {
+    const ctj = csvtojson();
+    let result = await ctj.fromFile(data);
+    return result;
+};
+
+router.post("/sendFile", async (req, res, next) => {
+    try {
+        // let data = await ctj.fromFile(req.files[0].path);
+        let data = await format(req.files[0].path);
+        res.send({
+            data,
+            total: data.length,
+        });
+    } catch (error) {
+        res.send({
+            message: "错误",
+        });
+    }
+    // res.send('上传成功')
+});
 module.exports = router;
