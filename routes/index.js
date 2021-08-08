a4e21e3e0ed75fb07fa553eb55d771f4ffe8d80c
shuaxin <986384495@qq.com>
2021-08-08 09:51:43 +0800

    feat:实现批量下载

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 const csvtojson = require("csvtojson");
 const axios = require("axios");
 const buffer = require("buffer").Buffer;
-const fs = require("fs").promises;
+const fs = require("fs");
 const path = require("path");
+const archiver = require("archiver");
+const archive = archiver("zip", {
+    zlib: { level: 9 }, // Sets the compression level.
+});
 /* GET home page. */
 router.get("/", function (req, res, next) {
     res.render("index", {
@@ -32,21 +36,48 @@ router.post("/sendFile", async (req, res, next) => {
         });
     }
 });
-
-router.post("/dowAll", async (req, res, next) => {
-    console.log(fs);
-    const { data } = req.body;
-    let responese = await axios.get(data[0].url, {
+// buffer下载单个文件
+const saveByBuffer = async (url) => {
+    let responese = await axios.get(url, {
         // 设置以buffer形式接收数据
         responseType: "arraybuffer",
     });
     // 转为buffer
     let resData = await buffer.from(responese.data);
     // 存储到本地
-    await fs.writeFile(
+    await fs.promises.writeFile(
         `${path.join(__dirname, "../static")}${responese.request.path}`,
         resData
     );
+};
+// stream下载单个文件
+const saveByStream = async (url) => {
+    let responese = await axios.get(url, {
+        responseType: "stream",
+    });
+    const ws = fs.createWriteStream(
+        `${path.join(__dirname, "../static")}${responese.request.path}`
+    );
+    await responese.data.pipe(ws);
+};
+
+router.post("/dowAll", async (req, res, next) => {
+    const { data } = req.body;
+    // await saveByBuffer(data[0].url);
+    // await saveByStream(data[0].url);
+    const ws = fs.createWriteStream(
+        path.join(__dirname, "../static") + "/allImg.zip"
+    );
+    archive.pipe(ws);
+    for (let i = 0; i < data.length; i++) {
+        let responese = await axios.get(data[i].url, {
+            responseType: "stream",
+        });
+        await archive.append(responese.data, {
+            name: responese.request.path.split("/")[1],
+        });
+    }
+    archive.finalize();
     res.send({
         msg: "ok",
     });
